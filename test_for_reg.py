9c08fe18be60452e2e26f4dcd9d0c4084dc8e076
JoepNoordhoek <j.noordhoek02@gmail.com>
2023-03-23 13:47:58 +0100

    Update test_for_reg.py

--- test_for_reg.py
+++ test_for_reg.py
@@ -1,7 +1,4 @@
-# import numpy as np
 from regions import Regions
-# import pyregion
-
 
 path = r'C:\Users\joepn\OneDrive\Documenten\BRP\data'
 
@@ -10,21 +7,40 @@ testreg2 = r'C:\Users\joepn\OneDrive\Documenten\BRP\data\2MASSJ12560830-6926539\
 
 regread = Regions.read(testreg, format='ds9')
 regread2 = Regions.read(testreg2, format='ds9')
-# regopen = pyregion.open(testreg)
 
-# reg = open(testreg)
-print('string',str(regread))
-print(type(regread))
-# print(regopen)
 
-x= float(str(regread)[38:47])
-y= float(str(regread)[51:60])
-print(x,y)
+def find_x_y_reg(file):
+    ''' Opens the .reg file and returns the x and y coordinate in the file. loc_circle is the number of the line
+    where the information on the circle is stored and sep is the thing used for seperating the x and y coord. '''
+    loc_circle = 3
+    sep = ','
+    with open(file, 'rt') as reg:
+        data_reg = reg.readlines()
+        data_reg = data_reg[loc_circle]
+    
+    first, second = data_reg.find(sep), data_reg.rfind(sep)
+    return float(data_reg[7:first]), float(data_reg[(first+1):second])
 
-print('string',str(regread2))
-x2= float(str(regread2)[38:47])
-y2= float(str(regread2)[51:60])
-print(x2,y2)
-#die godverdomme pyregion wilt niet installeren
 
-# x=540.46633, y=502.95775), radius=6.6979685)>]
+
+x, y = find_x_y_reg(testreg)
+print('xpuck',x)
+print('ypuck',y)
+
+print('')
+
+print('regread as string:',str(regread))
+print('type:',type(regread))
+
+print('')
+
+x= float(str(regread)[38:47])+1
+y= float(str(regread)[51:60])+1
+print('xjoep',x)
+print('yjoep',y)
+
+# print('string',str(regread2))
+# x2= float(str(regread2)[38:47])
+# y2= float(str(regread2)[51:60])
+# print(x2,y2)
+
