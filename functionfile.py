c6f7c6f374c394a4057461779d29c180db693584
puck241 <puck.rooijakkers@gmail.com>
2023-04-03 12:24:34 +0200

    trying to make code easier

--- functionfile.py
+++ functionfile.py
@@ -3,11 +3,20 @@ import matplotlib.pyplot as plt
 import astropy.io.fits as f
 from os import listdir
 
-# path = '/home/puck/Documents/BRP/data' #Puck
-path = r'C:\Users\joepn\OneDrive\Documenten\BRP\data' #Joep
+path = '/home/puck/Documents/brp/data' #Puck
+#path = r'C:\Users\joepn\OneDrive\Documenten\BRP\data' #Joep
 
+#%%
+#------------------------Retrieving data---------------------------------
 def star_info(star_list, idx):
-    ''' Returns the name of the star and date of observation as a tuple. '''
+    """ Returns the name of the star and date of observation
+    
+    :param star_list: A list of the names of the stars in the directory
+    :type star_list: list
+    :param idx: The index of the star you want in star_list
+    :type idx: int
+    :returns: The name of the file at idx and the data of observation 
+    :rtype: str, str"""
     return star_list[idx], listdir(f'{path}/{star_list[idx]}')[0]
 
 def star_data(star_list):
@@ -21,7 +30,77 @@ def star_data(star_list):
         arr[i] = data[0]
     return arr
 
+#%%
+#-------------------First data reduction----------------------------------
+def mask_but_center(arr):
+    ''' Masks everything but the area with radius r in a circle around the 
+    center. '''
+    x = y = np.linspace(-(len(arr))/2, (len(arr))/2, len(arr))
+    x_grid, y_grid = np.meshgrid(x, y)
+    r_grid = np.sqrt(x_grid**2 + y_grid**2)
+    arr[r_grid > 100] = None 
+    return arr
 
+def normalize(arr):
+    ''' Normalizes the array '''
+    arr = np.array(arr,dtype=np.float64)
+    return arr/np.nanmax(arr)
 
+def data_reduction(arr):
+    ''' Masks and then normalizes an array with one image. '''
+    mask_arr = mask_but_center(arr)
+    norm_arr = normalize(mask_arr)
+    return norm_arr
 
+#%%
+#--------------Finding the perfect median star------------------------------
+def mask_aperture(arr):
+    """ Creates a ring in arr between r = 30 and r = 50
+    
+    :param arr: An image of a star
+    :type arr: numpy.ndarray
+    :returns: The image where everything is masked but a ring at 30 < r < 50
+    :rtype: numpy.ndarray """
+    x = y = np.linspace(-(len(arr))/2, (len(arr))/2, len(arr))
+    x_grid, y_grid = np.meshgrid(x, y)
+    r_grid = np.sqrt(x_grid**2 + y_grid**2)
+    arr[r_grid > 50] = None 
+    arr[r_grid < 30] = None
+    return arr
 
+def arr_sort_rms(arr):
+    """ Returns array of star images sorted from best to worst rms
+    
+    :param arr: Array containing images of stars
+    :type arr: numpy.ndarray
+    :returns:  arr sorted from best to worst rms
+    :rtype: numpy.ndarray """
+    rms_arr = np.nanstd(arr, axis = (1, 2)) #arr with rms of every image
+    idx = np.argsort(rms_arr) #indexes sorted from best to worst rms
+    return arr[idx]
+
+def median_image(sorted_arr):
+    """ Creates a data cube where every ith image is the median of the first 
+    i+1 images in sorted_arr and created an array containing the rms of these
+    images 
+    
+    :param sorted_arr: Array containing images of stars sorted from best to 
+    worst rms
+    :type sorted_arr: numpy.ndarray
+    :returns: len(sorted_arr)x1024x1024 array where every ith image is the 
+    median of the first i+1 images in sorted_arr and an array with the rms
+    of these images. 
+    :rtype: numpy.ndarray, numpy.ndarray """
+    med_cube = np.zeros([sorted_arr.shape[0]-1, sorted_arr.shape[1], 
+                         sorted_arr.shape[2]]) #Cube containing all images. 
+    rms_med_im = np.zeros(sorted_arr.shape[0]-1)
+    
+    for i in range(2, len(sorted_arr)+1):
+        print(f'N = {i}')
+        N_im = sorted_arr[0:i]
+        med = np.median(N_im, axis = 0)
+        rms = np.nanstd(med)
+        rms_med_im[i-2] = rms
+        med_cube[i-2] = med
+    
+    return med_cube, rms_med_im
--- infofile.py
+++ infofile.py
@@ -1,28 +1,31 @@
-import functionfile as FF
-
 import numpy as np
 from os import listdir
 
-path = '/home/puck/Documents/BRP/data' #Puck
+#Path leading to the data
+path = '/home/puck/Documents/brp/data' #Puck
 # path = r'C:\Users\joepn\OneDrive\Documenten\BRP\data' #Joep
 
-#The images containing weird artifacts that make them useless
+#Array containing the names of all stars
+all_stars = np.array(sorted(listdir(path)))
+
+#Array containing the names of the stars that are useless because they are 
+#weird. 2MASSJ13032904-4723160 is star without flux image.
 weird_names = np.array(['2MASSJ11320835-5803199', '2MASSJ12041439-6418516', 
                         '2MASSJ12123577-5520273', '2MASSJ12192161-6454101', 
                         '2MASSJ12205449-6457242', '2MASSJ12472196-6808397',
                         '2MASSJ12582559-7028490', '2MASSJ13032904-4723160'])
 
-#2MASSJ13032904-4723160 is star without flux image
+#-----------------------------Binaries----------------------------------------
 
-#The binaries we found in our initial search
-bin1 = np.array(['2MASSJ11555771-5254008', '2MASSJ12560830-6926539', 
+#An array of the names of the binaries we found in search 1.
+bin1_names = np.array(['2MASSJ11555771-5254008', '2MASSJ12560830-6926539', 
                  '2MASSJ13015435-4249422', '2MASSJ13130714-4537438', 
                  'CD-43_3604', 'CVSO_751', 'Gaia_EDR3_3008386787098934144',
                  'HD_283629', 'HD_284266', 'HD_286179', 'UCAC4_454-011718', 
                  'UCAC4_475-014428', 'UCAC4_495-030196','UCAC4_501-011878'])
 
-#The binaries we found in our second search
-bin2 = np.array(['2MASSJ10065573-6352086','2MASSJ11272881-3952572',
+#An array of the names of the binaries we found in search 2.
+bin2_names = np.array(['2MASSJ10065573-6352086','2MASSJ11272881-3952572',
                  '2MASSJ11445217-6438548','2MASSJ11452016-5749094',
                  '2MASSJ12163007-6711477','2MASSJ12185802-5737191',
                  '2MASSJ12210808-5212226','2MASSJ12234012-5616325',
@@ -34,18 +37,24 @@ bin2 = np.array(['2MASSJ10065573-6352086','2MASSJ11272881-3952572',
                  'UCAC4_482-118442','UCAC4_496-013657','UCAC4_519-042115',
                  'V1319_TAU'])
 
-bin_names = np.append(bin1, bin2)
+#An array of the names of the binaries we found in search 3.
+#bin3_names = np.array([])
 
-#Data split in YSES and WISPIT data
-bin_wispit = np.array(['CoRoT_102718810', 'Gaia_EDR3_3014970387847972096',
+#An array containing the names of all binaries. 
+bin_names = np.append(bin1_names, bin2_names) #, bin3_names)
+
+#An array containing the names of all binary stars in the WiSPiT survey.
+bin_wp_names = np.array(['CoRoT_102718810', 'Gaia_EDR3_3014970387847972096',
                      'IRAS_08131-4432', 'RX_J2302.6+0034', 'UCAC4_127-038351',
                      'UCAC4_134-015937','UCAC4_406-011818','UCAC4_461-018326',
                      'UCAC4_482-118442', 'UCAC4_496-013657','UCAC4_519-042115',
-                     'V1319_TAU', 'CD-43_3604', 'CVSO_751', 'Gaia_EDR3_3008386787098934144', 
-                     'HD_283629', 'HD_284266', 'HD_286179', 'UCAC4_454-011718', 
-                     'UCAC4_475-014428', 'UCAC4_495-030196', 'UCAC4_501-011878'])
+                     'V1319_TAU', 'CD-43_3604', 'CVSO_751', 
+                     'Gaia_EDR3_3008386787098934144', 'HD_283629', 'HD_284266', 
+                     'HD_286179', 'UCAC4_454-011718', 'UCAC4_475-014428', 
+                     'UCAC4_495-030196', 'UCAC4_501-011878'])
 
-bin_yses = np.array(['2MASSJ10065573-6352086', '2MASSJ11272881-3952572',
+#An array containing the names of all binary stars in the YSES survey.
+bin_y_names = np.array(['2MASSJ10065573-6352086', '2MASSJ11272881-3952572',
                      '2MASSJ11445217-6438548', '2MASSJ11452016-5749094',
                      '2MASSJ12163007-6711477', '2MASSJ12185802-5737191',
                      '2MASSJ12210808-5212226', '2MASSJ12234012-5616325',
@@ -54,15 +63,21 @@ bin_yses = np.array(['2MASSJ10065573-6352086', '2MASSJ11272881-3952572',
                      '2MASSJ11555771-5254008', '2MASSJ12560830-6926539',
                      '2MASSJ13015435-4249422', '2MASSJ13130714-4537438',])
 
-sin_wispit = np.array(['2MASS_J05162151+1147472', '2MASS_J05264348+0143538',
-                       '2MASS_J06145339+0003000', 'GAIA_DR3_5854897321965963264',
-                       'HD_285372', 'HD_285579', 'HD_285778','HD_285840',
-                       'J_618A', 'NSVS_14407747', 'UCAC4_141-082231', 
-                       'UCAC4_200-015587', 'UCAC4_280-010722', 'UCAC4_312-058127',
-                       'UCAC4_319-026529', 'UCAC4_446-032370', 'UCAC4_537-015077',
-                       'V_V1267_CEN', 'V_V1346_Tau', 'V_V826_Tau'])
+#--------------------------------Single stars---------------------------------
 
-sin_yses = np.array(['2MASSJ10560422-6152054', '2MASSJ11175186-6402056',
+#An array containing the names of all single stars in the WiSPiT survey
+sin_wp_names = np.array(['2MASS_J05162151+1147472', '2MASS_J05264348+0143538',
+                       '2MASS_J06145339+0003000', 
+                       'GAIA_DR3_5854897321965963264', 'HD_285372', 
+                       'HD_285579', 'HD_285778','HD_285840','J_618A', 
+                       'NSVS_14407747', 'UCAC4_141-082231', 'UCAC4_200-015587', 
+                       'UCAC4_280-010722', 'UCAC4_312-058127',
+                       'UCAC4_319-026529', 'UCAC4_446-032370', 
+                       'UCAC4_537-015077', 'V_V1267_CEN', 'V_V1346_Tau', 
+                       'V_V826_Tau'])
+
+#An array containing the names of all single stars in the YSES survey
+sin_y_names = np.array(['2MASSJ10560422-6152054', '2MASSJ11175186-6402056',
                      '2MASSJ11275535-6626046', '2MASSJ11454278-5739285',
                      '2MASSJ12065276-5044463', '2MASSJ12090225-5120410',
                      '2MASSJ12101065-4855476', '2MASSJ12113142-5816533',
@@ -86,10 +101,9 @@ sin_yses = np.array(['2MASSJ10560422-6152054', '2MASSJ11175186-6402056',
                      '2MASSJ13444279-6347495', '2MASSJ13455599-5222255'])
 
 #Array containing the names of all single stars
-sin_names = np.append(sin_wispit, sin_yses)
+sin_names = np.append(sin_wp_names, sin_y_names)
 
-#Array containing the names of all stars
-all_stars = np.array(sorted(listdir(path)))
+
 
 
 
